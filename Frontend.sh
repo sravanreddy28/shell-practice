e8403b60d7a42c392911341c34b030a49a9dec0b
sravanreddy28 <sravankakireddy@gmail.com>
2023-11-17 12:54:35 +0530

    shell practice

--- Frontend.sh
+++ Frontend.sh
@@ -1,7 +1,5 @@
 dnf install nginx -y
-
-systemctl enable nginx
-systemctl start nginx
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 rm -rf /usr/share/nginx/html/*
 
@@ -10,4 +8,5 @@ cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
 
+systemctl enable nginx
 systemctl restart nginx
