b04e07059413936dcc79fe9d21b1f737935bf8d9
sravanreddy28 <sravankakireddy@gmail.com>
2023-12-11 16:30:25 +0530

    colour update

--- redis.sh
+++ redis.sh
@@ -1,14 +1,14 @@
 echo -e "\e[31m >>>>>> install repo <<<<<< \e[0m"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y >\tmp\roboshop.log
 
 echo -e "\e[32m >>>>>> enable redis <<<<<< \e[0m"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y >\tmp\roboshop.log
 
 echo -e "\e[33m >>>>>> install redis <<<<<< \e[0m"
-dnf install redis -y
+dnf install redis -y >\tmp\roboshop.log
 # update listen address /etc/redis.conf & vim /etc/redis/redis.conf
 echo -e "\e[34m >>>>>> enable redis  <<<<<< \e[0m"
-systemctl enable redis
+systemctl enable redis >\tmp\roboshop.log
 
 echo -e "\e[35m >>>>>> restart redis <<<<<< \e[0m"
-systemctl restart redis
+systemctl restart redis >\tmp\roboshop.log
