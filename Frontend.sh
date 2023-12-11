7bc16fa85cf801eee90e3076515321907fecaf55
sravanreddy28 <sravankakireddy@gmail.com>
2023-12-11 15:21:33 +0530

    colour update

--- Frontend.sh
+++ Frontend.sh
@@ -1,12 +1,23 @@
-echo -e "\e[31m >>>>>>  dnf install nginx -y <<<<<< \e[0m"
-echo -e "\e[32m >>>>>>  cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf <<<<<< \e[0m"
+echo -e "\e[31m >>>>>> install nginx -y <<<<<< \e[0m"
+dnf install nginx -y
 
-echo -e "\e[33m >>>>>>  rm -rf /usr/share/nginx/html/* <<<<<< \e[0m"
+echo -e "\e[32m >>>>>> cp nginx-roboshop.conf <<<<<< \e[0m"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[34m >>>>>>  curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip <<<<<< \e[0m"
-echo -e "\e[35m >>>>>>  cd /usr/share/nginx/html <<<<<< \e[0m"
+echo -e "\e[33m >>>>>> remove nginx demo  <<<<<< \e[0m"
+rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[36m >>>>>>  unzip /tmp/frontend.zip <<<<<< \e[0m"
+echo -e "\e[34m >>>>>> zip frontend <<<<<< \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[31m >>>>>>  systemctl enable nginx <<<<<< \e[0m"
-echo -e "\e[32m >>>>>>  systemctl restart nginx <<<<<< \e[0m"
+echo -e "\e[35m >>>>>> /usr/share/nginx/html <<<<<< \e[0m"
+cd /usr/share/nginx/html
+
+echo -e "\e[36m >>>>>> unzip frontend.zip <<<<<< \e[0m"
+ unzip /tmp/frontend.zip
+
+echo -e "\e[31m >>>>>> enable nginx <<<<<< \e[0m"
+systemctl enable nginx
+
+echo -e "\e[32m >>>>>> restart nginx <<<<<< \e[0m"
+systemctl restart nginx
