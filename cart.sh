b3fd3c5f6f5e5e829a121384a06eeb51fcb2b165
sravanreddy28 <sravankakireddy@gmail.com>
2023-12-11 15:54:53 +0530

    colour update

--- cart.sh
+++ cart.sh
@@ -1,24 +1,36 @@
-cp cart.service /etc/systemd/system/cart.service
+log=\tmp\roboshop.log
 
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+echo -e "\e[36m>>>>>>> cart service <<<<<<<<\e[0m"
+cp cart.service /etc/systemd/system/cart.service &>>${log}
 
-dnf install nodejs -y
+echo -e "\e[32m>>>>>>> nodejs repos download <<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 
-useradd roboshop
+echo -e "\e[33m>>>>>>> install  nodejs <<<<<<<\e[0m"
+yum install nodejs -y &>>${log}
 
-mkdir /app
+echo -e "\e[34m>>>>>>> add application user <<<<<<<\e[0m"
+useradd roboshop &>>${log}
 
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+echo -e "\e[35m>>>>>>> delete directory <<<<<<<\e[0m"
+rm -rf /app &>>${log}
+
+echo -e "\e[36m>>>>>>> create directory <<<<<<<\e[0m"
+mkdir /app &>>${log}
+
+echo -e "\e[32m>>>>>>> download artifacts <<<<<<<\e[0m"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>${log}
+
+echo -e "\e[33m>>>>>>> install artifacts <<<<<<<\e[0m"
 cd /app
-unzip /tmp/cart.zip
-
+unzip /tmp/cart.zip &>>${log}
 cd /app
-npm install
 
-dnf install mongodb-org-shell -y
-mongo --host mongodb.sdevops28.online </app/schema/cart.js
+echo -e "\e[36m>>>>>>> install dependencies <<<<<<<\e[0m"
+npm install &>>${log}
 
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
+echo -e "\e[35m>>>>>>> enable start cart <<<<<<<\e[0m"
+systemctl daemon-reload &>>${log}
+systemctl enable cart &>>${log}
+systemctl restart cart &>>${log}
+
