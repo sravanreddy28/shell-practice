4603fa704bfbd474db0104ac456709f4690ec588
sravanreddy28 <sravankakireddy@gmail.com>
2023-12-11 15:10:51 +0530

    colour update

--- Frontend.sh
+++ Frontend.sh
@@ -1,12 +1,12 @@
-dnf install nginx -y
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[31m >>>>>>  dnf install nginx -y <<<<<< \e[0m"
+echo -e "\e[32m >>>>>>  cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf <<<<<< \e[0m"
 
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[33m >>>>>>  rm -rf /usr/share/nginx/html/* <<<<<< \e[0m"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+echo -e "\e[34m >>>>>>  curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip <<<<<< \e[0m"
+echo -e "\e[35m >>>>>>  cd /usr/share/nginx/html <<<<<< \e[0m"
 
-unzip /tmp/frontend.zip
+echo -e "\e[36m >>>>>>  unzip /tmp/frontend.zip <<<<<< \e[0m"
 
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[31m >>>>>>  systemctl enable nginx <<<<<< \e[0m"
+echo -e "\e[32m >>>>>>  systemctl restart nginx <<<<<< \e[0m"
